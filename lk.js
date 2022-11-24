ef971a55e95e3e00cad63544dae749ab286a33ce
AlexDeng <a.denisov@elesta.ru>
2022-11-24 14:09:57 +0300

    add lk.js and first wok under form

---
+++ lk.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  const controlForm = document.forms.control;
+  const control__text = controlForm.controlText;
+  const checkBtn = controlForm.checkButton;
+
+  let ex = "G";
+  control__text.addEventListener("change", (e) => {
+    if (control__text.value === ex) {
+      console.log("Incorrect sign");
+    }
+  });
+
+  checkBtn.addEventListener("click", (e) => {
+    console.log(control__text.value);
+  });
+};
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function checkPassword() {
-  if (document.lk.pass.value === document.lk.repeat.value) {
-    document.getElementById("registration").style.visibility = "visible";
-    document.getElementById("false").style.visibility = "hidden";
-  } else document.getElementById("false").style.visibility = "visible";
-  document.getElementById("registration").style.visibility = "hidden";
-}
+// function checkPassword() {
+//   if (document.lk.pass.value === document.lk.repeat.value) {
+//     document.getElementById("registration").style.visibility = "visible";
+//     document.getElementById("false").style.visibility = "hidden";
+//   } else document.getElementById("false").style.visibility = "visible";
+//   document.getElementById("registration").style.visibility = "hidden";
+// }
