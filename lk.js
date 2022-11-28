9335fe8964d4dfe6357614fd6877e8c979e00107
Александр Денисов <C:\TCTitan14\AddOn\inet\TheBatPortable\Data\Mail>
2022-11-28 22:14:28 +0300

    Monday_changes

--- lk.js
+++ lk.js
@@ -1,12 +1,32 @@
 window.onload = function () {
   const controlForm = document.forms.control;
-  const control__text = controlForm.controlText;
-  const checkBtn = controlForm.checkButton;
+  // const checkBtn = controlForm.chkBtn;
+  let text = (document.querySelector("#symbol_check").value = "");
 
-  let ex = "G";
-  control__text.addEventListener("change", (e) => {
-    if (control__text.value === ex) {
-      console.log("Incorrect sign");
+  //   checkBtn.addEventListener("submit", (event) => {
+  //     event.preventDefault();
+  //     text = controlForm.controlText.value;
+  //     console.log(text);
+  //     if (text === "G") {
+  //       controlForm.controlText.innerHtml = "incorrect";
+  //       console.log("Bad guy");
+  //     } else {
+  //       controlForm.controlText.innerHtml = text;
+  //       console.log("Good guy");
+  //     }
+  //   });
+  // };
+
+  document.querySelector("#chkBtn").addEventListener("click", (event) => {
+    event.preventDefault();
+    text = document.querySelector("#symbol_check").value;
+    console.log(text);
+    if (text === "G") {
+      document.querySelector("#symbol_check").innerHtml = "incorrect";
+      console.log("Bad guy");
+    } else {
+      document.querySelector("#symbol_check").innerHtml = text;
+      console.log("Good guy");
     }
   });
 };
