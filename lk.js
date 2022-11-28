66ac728f5a588a7034c8e990d2fb8edd5aaf085c
Александр Денисов <C:\TCTitan14\AddOn\inet\TheBatPortable\Data\Mail>
2022-11-28 23:00:33 +0300

    fix & correct 1

--- lk.js
+++ lk.js
@@ -1,32 +1,16 @@
 window.onload = function () {
   const controlForm = document.forms.control;
-  // const checkBtn = controlForm.chkBtn;
-  let text = (document.querySelector("#symbol_check").value = "");
+  const checkBtn = controlForm.chkBtn;
 
-  //   checkBtn.addEventListener("submit", (event) => {
-  //     event.preventDefault();
-  //     text = controlForm.controlText.value;
-  //     console.log(text);
-  //     if (text === "G") {
-  //       controlForm.controlText.innerHtml = "incorrect";
-  //       console.log("Bad guy");
-  //     } else {
-  //       controlForm.controlText.innerHtml = text;
-  //       console.log("Good guy");
-  //     }
-  //   });
-  // };
-
-  document.querySelector("#chkBtn").addEventListener("click", (event) => {
+  checkBtn.addEventListener("click", (event) => {
     event.preventDefault();
-    text = document.querySelector("#symbol_check").value;
-    console.log(text);
+    let text = document.querySelector("#symbol_check").value;
     if (text === "G") {
-      document.querySelector("#symbol_check").innerHtml = "incorrect";
-      console.log("Bad guy");
+      document.querySelector("#symbol_check").value = "incorrect input, please, try again";
     } else {
-      document.querySelector("#symbol_check").innerHtml = text;
-      console.log("Good guy");
+      document.querySelector("#symbol_check").value = text;
     }
   });
 };
+
+// document.querySelector("#chkBtn") === document.forms.control.chkBtn
