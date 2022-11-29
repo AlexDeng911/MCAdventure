b289a1169123da3e9b4992264ed01afe81302990
AlexDeng <a.denisov@elesta.ru>
2022-11-29 14:20:46 +0300

    ver.2 inputs fix

--- lk.js
+++ lk.js
@@ -1,12 +1,30 @@
 window.onload = function () {
   const controlForm = document.forms.control;
-  const control__text = controlForm.controlText;
-  const checkBtn = controlForm.checkButton;
+  const checkBtn = controlForm.chkBtn;
+  const setColorButton = controlForm.colorBtn;
+  const setDateButton = controlForm.setDateButton;
 
-  let ex = "G";
-  control__text.addEventListener("change", (e) => {
-    if (control__text.value === ex) {
-      console.log("Incorrect sign");
+  checkBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    let text = document.querySelector("#symbol__check").value;
+    if (text === "G") {
+      document.querySelector("#symbol_check").value = "incorrect input, please, try again";
+    } else {
+      document.querySelector("#symbol_check").value = text;
     }
   });
+
+  setColorButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    let color = document.querySelector("#set__color").value;
+    document.querySelector(".colorNumber").innerHTML = color;
+  });
+
+  setDateButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    let dateRecord = document.querySelector("#set__date").value;
+    document.querySelector(".dateRecord").innerText = dateRecord;
+  });
 };
+
+// document.querySelector("#chkBtn") === document.forms.control.chkBtn
