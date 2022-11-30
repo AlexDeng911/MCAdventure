f9295e26ef527be89cd34092f36b78eb2bfc2db7
AlexDeng <a.denisov@elesta.ru>
2022-11-30 10:37:37 +0300

    stable version

--- lk.js
+++ lk.js
@@ -1,40 +1,30 @@
-window.onload = function () {
-  const controlForm = document.forms.control;
-  const checkBtn = controlForm.chkBtn;
-  const setColorButton = controlForm.colorBtn;
-  const setDateButton = controlForm.setDateButton;
+// window.onload = function () {
+//   const controlForm = document.forms.control;
+//   const checkBtn = controlForm.chkBtn;
+//   const setColorButton = controlForm.colorBtn;
+//   const setDateButton = controlForm.setDateButton;
 
-  checkBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    let text = document.querySelector("#symbol__check").value;
-    if (text === "G") {
-      document.querySelector("#symbol_check").value = "incorrect input, please, try again";
-    } else {
-      document.querySelector("#symbol_check").value = text;
-    }
-  });
+//   checkBtn.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     let text = document.querySelector("#symbol__check").value;
+//     if (text === "G") {
+//       document.querySelector("#symbol__check").value = "incorrect input, please, try again";
+//     } else {
+//       document.querySelector("#symbol__check").value = text;
+//     }
+//   });
 
-  setColorButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    let color = document.querySelector("#set__color").value;
-    document.querySelector(".colorNumber").innerHTML = color;
-  });
+//   setColorButton.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     let color = document.querySelector("#set__color").value;
+//     document.querySelector(".colorNumber").innerHTML = color;
+//   });
 
-  setDateButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    let dateRecord = document.querySelector("#set__date").value;
-    document.querySelector(".dateRecord").innerText = dateRecord;
-  });
-
-  const regForm = document.forms.lk;
-  const sendReg = regForm.sendRegistration;
-
-  sendReg.addEventListener("click", (event) => {
-    event.preventDefault();
-    let login = regForm.login.value;
-    // regForm.login.value = login;
-    console.log(login);
-  });
-};
+//   setDateButton.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     let dateRecord = document.querySelector("#set__date").value;
+//     document.querySelector(".dateRecord").innerText = dateRecord;
+//   });
+// };
 
 // document.querySelector("#chkBtn") === document.forms.control.chkBtn
--- script.js
+++ script.js
@@ -1,18 +1,52 @@
-// function checkPassword() {
-//   if (document.lk.pass.value === document.lk.repeat.value) {
-//     document.getElementById("registration").style.visibility = "visible";
-//     document.getElementById("false").style.visibility = "hidden";
-//   } else document.getElementById("false").style.visibility = "visible";
-//   document.getElementById("registration").style.visibility = "hidden";
-// }
-// window.onload = function () {
-//   const regForm = document.forms.lk;
-//   const sendReg = regForm.sendRegistration;
+window.onload = function () {
+  const regForm = document.forms.lk;
+  const sendReg = regForm.sendRegistration;
 
-//   sendReg.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     let login = document.querySelector("#login").value;
-//     document.querySelector('#login').value = login;
-//     console.log(login);
-//   });
-// };
+  sendReg.addEventListener("click", (event) => {
+    event.preventDefault();
+    let login = document.querySelector("#login").value;
+    let email = document.querySelector("#email").value;
+    let pass = document.querySelector("#pass").value;
+    let confirm = document.querySelector("#confirm__pass").value;
+
+    document.querySelector("#login").innerText = login;
+    document.querySelector("#email").innerText = email;
+    if (pass === confirm) {
+      document.querySelector("#registration").innerText = "* Registration - OK";
+    } else {
+      document.querySelector("#registration").innerText = "* password is not the same ";
+    }
+    console.log(login, email, pass);
+  });
+
+  const controlForm = document.forms.control;
+  const checkBtn = controlForm.chkBtn;
+  const setColorButton = controlForm.colorBtn;
+  const setDateButton = controlForm.setDateButton;
+
+  checkBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    let text = document.querySelector("#symbol__check").value;
+    console.log(text)
+    if (text === "G") {
+      document.querySelector("#symbol__check").value = "incorrect input, please, try again";
+    } else {
+      document.querySelector("#symbol__check").value = text;
+    }
+  });
+
+  setColorButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    let color = document.querySelector("#set__color").value;
+    document.querySelector(".colorNumber").innerHTML = color;
+  });
+
+  setDateButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    let dateRecord = document.querySelector("#set__date").value;
+    document.querySelector(".dateRecord").innerText = dateRecord;
+  });
+};
+
+
+// submit - почему не работает?
