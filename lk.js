0311ac9dd8a9afd3cf29a766414bdce51e7d6af7
AlexDeng <a.denisov@elesta.ru>
2022-12-01 14:00:16 +0300

    add phone input & fix bugs

--- lk.js
+++ lk.js
@@ -1,30 +1,28 @@
-// window.onload = function () {
-//   const controlForm = document.forms.control;
-//   const checkBtn = controlForm.chkBtn;
-//   const setColorButton = controlForm.colorBtn;
-//   const setDateButton = controlForm.setDateButton;
+const controlForm = document.forms.control;
+const checkBtn = controlForm.chkBtn;
+const setColorButton = controlForm.colorBtn;
+const setDateButton = controlForm.setDateButton;
 
-//   checkBtn.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     let text = document.querySelector("#symbol__check").value;
-//     if (text === "G") {
-//       document.querySelector("#symbol__check").value = "incorrect input, please, try again";
-//     } else {
-//       document.querySelector("#symbol__check").value = text;
-//     }
-//   });
+checkBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    let text = document.querySelector("#symbol__check").value;
+    if (text === "G") {
+        document.querySelector("#symbol__check").value = "incorrect input, please, try again";
+    } else {
+        document.querySelector("#symbol__check").value = text;
+    }
+});
 
-//   setColorButton.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     let color = document.querySelector("#set__color").value;
-//     document.querySelector(".colorNumber").innerHTML = color;
-//   });
+setColorButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    let color = document.querySelector("#set__color").value;
+    document.querySelector(".colorNumber").innerHTML = color;
+});
 
-//   setDateButton.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     let dateRecord = document.querySelector("#set__date").value;
-//     document.querySelector(".dateRecord").innerText = dateRecord;
-//   });
-// };
+setDateButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    let dateRecord = document.querySelector("#set__date").value;
+    document.querySelector(".dateRecord").innerText = dateRecord;
+});
 
-// document.querySelector("#chkBtn") === document.forms.control.chkBtn
+// document.querySelector("#chkBtn") === document.forms.control.chkBtn;
--- script.js
+++ script.js
@@ -1,52 +1,21 @@
-window.onload = function () {
-  const regForm = document.forms.lk;
-  const sendReg = regForm.sendRegistration;
+const regForm = document.forms.lk;
+const sendReg = regForm.sendRegistration;
 
-  sendReg.addEventListener("click", (event) => {
+sendReg.addEventListener("click", (event) => {
     event.preventDefault();
     let login = document.querySelector("#login").value;
     let email = document.querySelector("#email").value;
     let pass = document.querySelector("#pass").value;
     let confirm = document.querySelector("#confirm__pass").value;
+    let phone = document.querySelector("#phone").value;
 
     document.querySelector("#login").innerText = login;
     document.querySelector("#email").innerText = email;
+    document.querySelector("#phone").innerText = phone;
     if (pass === confirm) {
-      document.querySelector("#registration").innerText = "* Registration - OK";
+        document.querySelector("#registration").innerText = "* Registration - OK";
     } else {
-      document.querySelector("#registration").innerText = "* password is not the same ";
+        document.querySelector("#registration").innerText = "* password is not the same ";
     }
-    console.log(login, email, pass);
-  });
-
-  const controlForm = document.forms.control;
-  const checkBtn = controlForm.chkBtn;
-  const setColorButton = controlForm.colorBtn;
-  const setDateButton = controlForm.setDateButton;
-
-  checkBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    let text = document.querySelector("#symbol__check").value;
-    console.log(text)
-    if (text === "G") {
-      document.querySelector("#symbol__check").value = "incorrect input, please, try again";
-    } else {
-      document.querySelector("#symbol__check").value = text;
-    }
-  });
-
-  setColorButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    let color = document.querySelector("#set__color").value;
-    document.querySelector(".colorNumber").innerHTML = color;
-  });
-
-  setDateButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    let dateRecord = document.querySelector("#set__date").value;
-    document.querySelector(".dateRecord").innerText = dateRecord;
-  });
-};
-
-
-// submit - почему не работает?
+    console.log(login, email, phone, pass);
+});
